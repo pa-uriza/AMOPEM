d942a2f4c83a832bf5c0623d138fdad8e88d6e3c
pa-uriza <pa.uriza274@uniandes.edu.co>
2018-07-24 11:35:23 -0500

    Data wrangle

--- Scripts/DataRead.R
+++ Scripts/DataRead.R
@@ -3,6 +3,7 @@ library(readxl)
 library(lubridate)
 library(glue)
 
+# Precios
 anios<-1995:2017
 Pr<-tibble()
 for(y in anios){
@@ -12,6 +13,18 @@ for(y in anios){
 }
 Pr<-Pr[-26]
 
-Pr <- Pr %>% gather(Hora,Precio,-Fecha) %>% transmute(DTTM = Fecha + hours(Hora),Precio) %>% arrange(DTTM)
+Pr <- Pr %>% gather(Hora,Precio,-Fecha) %>% transmute(DTTM = Fecha + hours(Hora),Price = Precio) %>% arrange(DTTM)
 
-write.csv(Pr,glue("Data/Processed/Precio_Bolsa_Nacional_($kwh)_{anios[1]}-{anios[length(anios)]}.csv"))
+write_csv(Pr,glue("Data/Processed/Precio_Bolsa_Nacional_($kwh)_{anios[1]}-{anios[length(anios)]}.csv"))
+
+# Imputacion precios en cero
+
+Pr2 <- Pr %>% mutate(Price = if_else(Price == 0,NA_real_,Price)) %>% fill(Price,.direction = "up")
+
+write_csv(Pr2,glue("Data/Processed/Precio_Bolsa_Nacional_($kwh)_{anios[1]}-{anios[length(anios)]}_Imp.csv"))
+
+# Fechas festivos
+Festivos <- read_csv("Data/Raw/Festivos_1995-2018.csv",locale = locale(encoding = stringi::stri_enc_get())) %>%
+  gather(-Asignacion,-`Fecha base`, -Fiesta,key = "Año",value = "Fecha")
+
+write_csv(Festivos,glue("Data/Processed/Festivos_1995-2018.csv"))
